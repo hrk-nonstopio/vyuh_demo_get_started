3caac15bade88e784d7c6c1e95ac4d6c46597ac2
Hrishikesh Kadam <hrishikesh.kadam@nonstopio.com>
2024-11-06 17:22:34 +0530

    Copy paste lib/main.dart from https://docs.vyuh.tech/intro/integrating-cms/#5-setup-the-vyuh-app

--- frontend_flutter/lib/main.dart
+++ frontend_flutter/lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:sanity_client/client.dart';
 import 'package:vyuh_core/vyuh_core.dart' as vc;
+import 'package:vyuh_extension_content/vyuh_extension_content.dart';
+import 'feature.dart' as blog;
+import 'package:vyuh_feature_system/vyuh_feature_system.dart' as system;
 import 'package:vyuh_feature_developer/vyuh_feature_developer.dart'
     as developer;
-import 'feature.dart';
+import 'package:vyuh_plugin_content_provider_sanity/vyuh_plugin_content_provider_sanity.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   vc.runApp(
-    initialLocation: '/counter',
+    initialLocation: '/blog',
     features: () => [
-      feature,
+      system.feature,
       developer.feature,
+      blog.feature,
     ],
+    plugins: PluginDescriptor(
+      content: DefaultContentPlugin(
+        provider: SanityContentProvider(
+          SanityClient(
+            SanityConfig(
+              dataset: 'your_dataset',
+              projectId: 'your_project_id',
+              token:'your_token',
+            ),
+          ),
+        ),
+      )
+    ),
   );
 }
