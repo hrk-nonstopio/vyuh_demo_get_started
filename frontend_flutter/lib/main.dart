79ac65f50655a241d147252b7bd40afc97b0dee7
Hrishikesh Kadam <hrishikesh.kadam@nonstopio.com>
2024-11-06 20:05:09 +0530

    Add projectId, dataset, token to SanityConfig

--- frontend_flutter/lib/main.dart
+++ frontend_flutter/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:sanity_client/client.dart';
 import 'package:vyuh_core/plugin/plugin_descriptor.dart';
 import 'package:vyuh_core/vyuh_core.dart' as vc;
 import 'package:vyuh_extension_content/vyuh_extension_content.dart';
-import 'feature.dart' as blog;
+import 'blog/feature.dart' as blog;
 import 'package:vyuh_feature_system/vyuh_feature_system.dart' as system;
 import 'package:vyuh_feature_developer/vyuh_feature_developer.dart'
     as developer;
@@ -24,11 +24,13 @@ void main() async {
         provider: SanityContentProvider(
           SanityClient(
             SanityConfig(
-              dataset: 'your_dataset',
-              projectId: 'your_project_id',
-              token: 'your_token',
+              projectId: 'w5o9x6m2',
+              dataset: 'production',
+              token:
+                  'skZhohtdospcpQi8QdwtYQRe5ohr7OUljkoeN51d4P9drqLsclgbhE1dZkbddM64g9cUZyLrPvELqMUKya17lUtxnBTm4XhTBWjeNkA0D1IyQscCsA4orB847ZxsmxRYnotTKiTAQWief26g4FZ9oKQha8mOn77R4y9ZIbXQPkqBbLiZZb8C',
             ),
           ),
+          cacheDuration: const Duration(seconds: 5),
         ),
       ),
     ),
